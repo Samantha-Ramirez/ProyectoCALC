388beb375f8445b79cfb8b16c046062e9c10cfa3
Samantha-Ramirez <samantha.p.ramirez.y@gmail.com>
2025-07-21 15:37:39 -0400

    update: Funciones principales y librerias

---
+++ main.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+from tkinter import ttk
+import numpy as np
+from playsound import playsound
+
+def pressDigit():
+    return
+
+def getDigitSound():
+    return
+
+def getSinSignal():
+    return
+
+def getSinSignalGraph():
+    return
+
+def getFrequenciesGraph():
+    return
+
+def playSignal():
+    return
+
+def decodeSignal():
+    return
+
+def graphSignal():
+    return
